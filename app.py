a9f76ed4024f719a8b6690e56dfa1ff600263f8e
Krishnavamsireddy2004 <vtu23008@veltech.edu.in>
2026-03-22 14:59:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+
+st.title("🌿 AI Crop Health Detection System")
+
+image_file = st.file_uploader("Upload Leaf Image", type=["jpg", "png"])
+audio_file = st.file_uploader("Upload Pest Audio (Optional)", type=["wav"])
+
+if image_file:
+    image = Image.open(image_file)
+    st.image(image)
+
+    diseases = ["Tomato Early Blight", "Healthy", "Leaf Spot"]
+    disease = np.random.choice(diseases)
+
+    severity = np.random.choice(["Mild", "Moderate", "Severe"])
+
+    st.write("🌿 Disease:", disease)
+    st.write("📊 Severity:", severity)
+
+    pest = "Not Detected"
+    if audio_file:
+        pest = np.random.choice(["Detected", "Not Detected"])
+
+    st.write("🐛 Pest Status:", pest)
+
+    if severity == "Severe" and pest == "Detected":
+        final = "🚨 HIGH RISK"
+    elif severity == "Moderate":
+        final = "⚠️ MODERATE RISK"
+    else:
+        final = "✅ LOW RISK"
+
+    st.write("🔥 Final Result:", final)
+
+    if severity == "Mild":
+        solution = "Use organic spray"
+    elif severity == "Moderate":
+        solution = "Apply fungicide"
+    else:
+        solution = "Use strong pesticide immediately"
+
+    st.write("💊 Solution:", solution)
